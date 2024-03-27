4fcd063a8eccee33859687c2c09fad50eb8debdf
tenraneko <nekocubot@gmail.com>
2024-03-27 21:19:09 +0500

    test

---
+++ lib/types/ssstik.d.ts
@@ -0,0 +1,28 @@
+export type SSSTikFetchTT = {
+    status: "success" | "error";
+    message?: string;
+    result?: string;
+};
+export type SSSTikResponse = {
+    status: "success" | "error";
+    message?: string;
+    result?: {
+        type: "image" | "video";
+        desc: string;
+        author: Author;
+        statistics: Statistics;
+        images?: string[];
+        video?: string;
+        cover?: string | null;
+        music: string;
+    };
+};
+export type Author = {
+    avatar: string;
+    nickname: string;
+};
+export type Statistics = {
+    likeCount: string;
+    commentCount: string;
+    shareCount: string;
+};
